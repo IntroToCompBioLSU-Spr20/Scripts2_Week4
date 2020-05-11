5d1afa339a9ad56d9d9003c9aa02703cd517e444
jembrown <jeremymbrown@gmail.com>
2020-05-11 01:05:03 -0500

    rename Homyra hw

--- Week4Assignments/Week4Assignment_Homyra.sh
+++ Week4Assignments/Week4Assignment_Homyra.sh
@@ -2,4 +2,4 @@
 
 #The script depicts the last 248 lines out of 249 lines of the file named assignment 3 and will accept any Hot alignment score at the end of each line and any number of taxa. 
 
-tail -n248 assignment3 | grep $1$ | grep $2',0.' | awk -F"," '{print $2}'
+tail -n248 chiari.summary_statistics.csv | grep $1$ | grep $2',0.' | awk -F"," '{print $2}'
